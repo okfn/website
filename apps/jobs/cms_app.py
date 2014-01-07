06830b84693bb79375d61883f087dab503538709
Nick Stenning <nick@whiteink.com>
2014-01-07 20:22:46 +0100

    Allow app to be bound to CMS pages

---
+++ apps/jobs/cms_app.py
@@ -0,0 +1,9 @@
+from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
+from django.utils.translation import ugettext_lazy as _
+
+class JobsAppHook(CMSApp):
+    name = _("Jobs")
+    urls = ["apps.jobs.urls"]
+
+apphook_pool.register(JobsAppHook)
