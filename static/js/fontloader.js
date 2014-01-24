65c7c3811576f1c86b9b4e6373a36ade9fcd090e
Nick Stenning <nick@whiteink.com>
2014-01-24 17:01:28 +0100

    templates: Basic layout and styling for main header

---
+++ static/js/fontloader.js
@@ -0,0 +1,9 @@
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
