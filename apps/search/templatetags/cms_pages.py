3a7830bc5c32c81ae3109012fe41f91b26061445
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2014-02-20 11:18:58 +0000

    search: catch exception if placeholder doesn't exist

--- apps/search/templatetags/cms_pages.py
+++ apps/search/templatetags/cms_pages.py
@@ -1,5 +1,7 @@
 from django import template
 from cms.models.pluginmodel import CMSPlugin
+from cms.models.placeholdermodel import Placeholder
+
 from django.utils.safestring import mark_safe
 
 register = template.Library()
@@ -9,7 +11,10 @@ register = template.Library()
 def placeholder_content(page, slot='blurb'):
     result = ''
 
-    page_placeholders = page.placeholders.get(slot=slot)
+    try:
+        page_placeholders = page.placeholders.get(slot=slot)
+    except Placeholder.DoesNotExist:
+        return result
     for plugin in CMSPlugin.objects.filter(placeholder=page_placeholders):
         instance, plugin_type = plugin.get_plugin_instance()
         if instance is not None:
