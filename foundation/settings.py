0a6f0f29afebd37964f91a5dff71e549fe4d1b12
Nick Stenning <nick@whiteink.com>
2014-02-24 23:28:38 +0000

    security: Add NetDNA CDN for fontawesome fonts

--- foundation/settings.py
+++ foundation/settings.py
@@ -303,7 +303,8 @@ if env.get('DJANGO_CSP_REPORT_URI') is not None:
     CSP_STYLE_SRC = asset_hosts + ['https://fonts.googleapis.com',
                                    "'unsafe-inline'"]
     CSP_IMG_SRC = asset_hosts + ['data://']
-    CSP_FONT_SRC = asset_hosts + ['https://themes.googleusercontent.com']
+    CSP_FONT_SRC = asset_hosts + ['https://netdna.bootstrapcdn.com',
+                                  'https://themes.googleusercontent.com']
 
     CSP_REPORT_URI = env.get('DJANGO_CSP_REPORT_URI')
 
