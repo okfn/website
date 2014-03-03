d0ab7ecae1cc917d11e6e1381fd7d1c2ba48cede
Nick Stenning <nick@whiteink.com>
2014-03-03 18:45:24 +0100

    press: Don't be so defensive
    There is no good reason why this should ever throw (the release_date
    field is not nullable, and non-dates shouldn't use this filter) and so
    in order to catch errors early, remove the preemptive error handling
    here.

--- foundation/press/templatetags/press.py
+++ foundation/press/templatetags/press.py
@@ -7,8 +7,5 @@ register = template.Library()
 
 @register.filter
 def active_release(release_date):
-    try:
-        week_ago = timezone.now() - timedelta(weeks=1)
-        return release_date > week_ago
-    except:
-        return False
+    week_ago = timezone.now() - timedelta(weeks=1)
+    return release_date > week_ago
