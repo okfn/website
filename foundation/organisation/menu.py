5bcfba269faf532ca88e6555369126c10337a90b
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2014-03-24 12:57:41 +0000

    organisation: attach menu to the network groups' cms app
    This makes breadcrumbs work (and it is organised so that regional
    groups are sub pages of country groups).

--- foundation/organisation/menu.py
+++ foundation/organisation/menu.py
@@ -2,7 +2,7 @@ from menus.base import NavigationNode
 from menus.menu_pool import menu_pool
 from django.utils.translation import ugettext_lazy as _
 from cms.menu_bases import CMSAttachMenu
-from .models import Project
+from .models import Project, NetworkGroup
 
 
 class ProjectMenu(CMSAttachMenu):
@@ -21,3 +21,31 @@ class ProjectMenu(CMSAttachMenu):
         return nodes
 
 menu_pool.register_menu(ProjectMenu)
+
+
+class NetworkGroupMenu(CMSAttachMenu):
+
+    name = _('Network Group')
+
+    def get_nodes(self, request):
+        nodes = []
+        for group in NetworkGroup.objects.countries():
+            node = NavigationNode(
+                group.get_country_display(),
+                group.get_absolute_url(),
+                group.pk,
+            )
+            nodes.append(node)
+
+            for regiongroup in NetworkGroup.objects\
+                    .regions(country=group.country_slug):
+                node = NavigationNode(
+                    regiongroup.region,
+                    regiongroup.get_absolute_url(),
+                    regiongroup.pk,
+                    group.pk,
+                    )
+                nodes.append(node)
+        return nodes
+
+menu_pool.register_menu(NetworkGroupMenu)
