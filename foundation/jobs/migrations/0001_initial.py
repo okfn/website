947085becc9265ac6864dc4441a7bacdc80e36cc
Nigel Babu <nigel@nigelb.me>
2016-01-27 16:20:55 +0530

    Add migrations for jobs app

--- foundation/jobs/migrations/0001_initial.py
+++ foundation/jobs/migrations/0001_initial.py
@@ -1,38 +1,28 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Job'
-        db.create_table(u'jobs_job', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=150)),
-            ('description', self.gf('django.db.models.fields.TextField')()),
-            ('submission_email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
-            ('submission_closes', self.gf('django.db.models.fields.DateTimeField')()),
-        ))
-        db.send_create_signal(u'jobs', ['Job'])
+    dependencies = [
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Job'
-        db.delete_table(u'jobs_job')
-
-
-    models = {
-        u'jobs.job': {
-            'Meta': {'object_name': 'Job'},
-            'description': ('django.db.models.fields.TextField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'submission_closes': ('django.db.models.fields.DateTimeField', [], {}),
-            'submission_email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '150'})
-        }
-    }
-
-    complete_apps = ['jobs']
+    operations = [
+        migrations.CreateModel(
+            name='Job',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('title', models.CharField(max_length=150)),
+                ('description', models.TextField()),
+                ('submission_email', models.EmailField(max_length=254)),
+                ('submission_closes', models.DateTimeField()),
+            ],
+            options={
+                'ordering': ('submission_closes',),
+            },
+        ),
+    ]
