88d3af3fcf0b7e06a9f299e72d5b753b8047f9c6
Nigel Babu <nigel@nigelb.me>
2016-02-23 10:38:10 +0530

    Upgrade Djstatic

--- lib/dj_static.py
+++ lib/dj_static.py
@@ -4,9 +4,7 @@ import static
 
 from django.conf import settings
 from django.core.handlers.wsgi import WSGIHandler
-from django.core.handlers.base import get_path_info
-from django.contrib.staticfiles.handlers import\
-    StaticFilesHandler as DebugHandler
+from django.contrib.staticfiles.handlers import StaticFilesHandler as DebugHandler
 
 try:
     from urllib.parse import urlparse
@@ -14,19 +12,44 @@ except ImportError:     # Python 2
     from urlparse import urlparse
 from django.contrib.staticfiles import utils
 
+try:
+    from django.core.handlers.wsgi import get_path_info
+except ImportError:  # django < 1.7
+    try:
+        from django.core.handlers.base import get_path_info
+    except ImportError:     # django < 1.5
+        import sys
+        py3 = sys.version_info[0] == 3
+
+        def get_path_info(environ):
+            """
+            Returns the HTTP request's PATH_INFO as a unicode string.
+            """
+            path_info = environ.get('PATH_INFO', str('/'))
+            # Under Python 3, strings in environ are decoded with ISO-8859-1;
+            # re-encode to recover the original bytestring provided by the web server.
+            if py3:
+                path_info = path_info.encode('iso-8859-1')
+            # It'd be better to implement URI-to-IRI decoding, see #19508.
+            return path_info.decode('utf-8')
+
 
 class Cling(WSGIHandler):
     """WSGI middleware that intercepts calls to the static files
     directory, as defined by the STATIC_URL setting, and serves those files.
     """
-    def __init__(self, application, base_dir=None):
+    def __init__(self, application, base_dir=None, base_url=None, ignore_debug=False):
         self.application = application
+        self.ignore_debug = ignore_debug
         if not base_dir:
             base_dir = self.get_base_dir()
-        self.base_url = urlparse(self.get_base_url())
+        self.base_url = urlparse(base_url or self.get_base_url())
 
         self.cling = static.Cling(base_dir)
-        self.debug_cling = DebugHandler(base_dir)
+        try:
+            self.debug_cling = DebugHandler(application, base_dir=base_dir)
+        except TypeError:
+            self.debug_cling = DebugHandler(application)
 
         super(Cling, self).__init__()
 
@@ -60,7 +83,7 @@ class Cling(WSGIHandler):
             return self.application(environ, start_response)
 
         # Serve static requests from static.Cling
-        if not self.debug:
+        if not self.debug or self.ignore_debug:
             environ = self._transpose_environ(environ)
             return self.cling(environ, start_response)
         # Serve static requests in debug mode from StaticFilesHandler
