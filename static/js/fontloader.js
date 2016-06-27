6a3d6c99ee648b3ff438c1a7fc0afd86d8eba97a
Knut Hühne <knut@viderum.com>
2016-06-27 11:09:24 +0200

    User typekit instead of google fonts

--- static/js/fontloader.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-    '://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
-})();
