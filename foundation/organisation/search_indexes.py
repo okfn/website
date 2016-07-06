b8b1bec4bfa5de791f2ba0c349cb681c469e5813
Knut Hühne <knut@viderum.com>
2016-07-06 13:46:46 +0200

    replace forgotten occurrences of mailinglist_url

--- foundation/organisation/search_indexes.py
+++ foundation/organisation/search_indexes.py
@@ -18,7 +18,7 @@ class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     twitter = indexes.CharField(model_attr='twitter')
     homepage_url = indexes.CharField(model_attr='homepage_url')
-    mailinglist_url = indexes.CharField(model_attr='mailinglist_url')
+    forum_url = indexes.CharField(model_attr='forum_url')
     sourcecode_url = indexes.CharField(model_attr='sourcecode_url')
 
     def get_model(self):
