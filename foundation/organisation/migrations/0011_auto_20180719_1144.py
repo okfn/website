6aa27677cf580d39bfad9e4ff244648dedd9b18a
amercader <amercadero@gmail.com>
2018-07-19 13:45:04 +0200

    Add migration for board order fields

---
+++ foundation/organisation/migrations/0011_auto_20180719_1144.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('organisation', '0010_networkgroup_forum_group_url'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='board',
+            options={'ordering': ['-order', 'name']},
+        ),
+        migrations.AlterModelOptions(
+            name='boardmembership',
+            options={'ordering': ['-order', 'person__name']},
+        ),
+        migrations.AddField(
+            model_name='board',
+            name='order',
+            field=models.IntegerField(help_text=b'Higher numbers mean higher up in the food chain', null=True, blank=True),
+        ),
+        migrations.AddField(
+            model_name='boardmembership',
+            name='order',
+            field=models.IntegerField(help_text=b'Higher numbers mean higher up in the food chain', null=True, blank=True),
+        ),
+    ]
