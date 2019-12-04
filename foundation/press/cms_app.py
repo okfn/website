8ebe202919aabea4951ce34adccaedfe98af5254
roll <eskarev@gmail.com>
2019-12-04 20:21:06 +0300

    Disable PressMention
    It causes an error and it doesn't seem in use at all

--- foundation/press/cms_app.py
+++ foundation/press/cms_app.py
@@ -2,7 +2,8 @@ from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
-from .menu import PressReleaseMenu, PressMentionMenu
+from .menu import PressReleaseMenu
+#  from .menu import PressMentionMenu
 
 
 class PressReleaseAppHook(CMSApp):
@@ -14,10 +15,10 @@ class PressReleaseAppHook(CMSApp):
 apphook_pool.register(PressReleaseAppHook)
 
 
-class PressMentionAppHook(CMSApp):
-    name = _("Press Mentions")
-    urls = ["foundation.press.urls.pressmentions"]
-    menus = [PressMentionMenu]
+#  class PressMentionAppHook(CMSApp):
+    #  name = _("Press Mentions")
+    #  urls = ["foundation.press.urls.pressmentions"]
+    #  menus = [PressMentionMenu]
 
 
-apphook_pool.register(PressMentionAppHook)
+#  apphook_pool.register(PressMentionAppHook)
