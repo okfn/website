87f3863d1528f80b9dd579b483faeea11580c7cc
chris48s <chris.shaw480@gmail.com>
2020-08-21 16:58:45 +0100

    rename menu.py files to cms_menus.py
    resolves
    DeprecationWarning: menu.py filename is deprecated, and it will be removed in version 3.4; please rename it to cms_menus.py

--- foundation/organisation/cms_app.py
+++ foundation/organisation/cms_app.py
@@ -1,7 +1,7 @@
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
-from .menu import ProjectMenu, ThemeMenu
+from .cms_menus import ProjectMenu, ThemeMenu
 
 
 class UnitsAppHook(CMSApp):
--- foundation/press/cms_app.py
+++ foundation/press/cms_app.py
@@ -3,7 +3,7 @@ from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
-from .menu import PressReleaseMenu
+from .cms_menus import PressReleaseMenu
 #  from .menu import PressMentionMenu
 
 
