e3fa47eb4b593c8f62ea080e02e465c6cb4ddc99
chris48s <chris.shaw480@gmail.com>
2020-08-21 16:58:41 +0100

    add SessionAuthenticationMiddleware to MIDDLEWARE_CLASSES
    resolves
    RemovedInDjango110Warning: Session verification will become mandatory in Django 1.10. Please add 'django.contrib.auth.middleware.SessionAuthenticationMiddleware' to your MIDDLEWARE_CLASSES setting when you are ready to opt-in after reading the upgrade considerations in the 1.8 release notes.

--- foundation/settings.py
+++ foundation/settings.py
@@ -154,6 +154,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'cms.middleware.page.CurrentPageMiddleware',
