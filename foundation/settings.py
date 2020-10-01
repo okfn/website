c20f62ca3708262bbec8c98a997104fdb1b77d09
chris48s <chris.shaw480@gmail.com>
2020-10-01 11:10:27 +0100

    remove redundant middleware

--- foundation/settings.py
+++ foundation/settings.py
@@ -154,7 +154,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'cms.middleware.page.CurrentPageMiddleware',
