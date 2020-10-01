e5f48d26bd5e42b82c06e2974c069d54108dc01a
chris48s <chris.shaw480@gmail.com>
2020-10-01 11:10:19 +0100

    aldryn_video: update tests/get them passing in-app

--- aldryn_video/tests.py
+++ aldryn_video/tests.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
-from cms.api import create_page, add_plugin
-from cms.test_utils.testcases import (CMSTestCase, URL_CMS_PLUGIN_EDIT)
-from aldryn_video.cms_plugins import OEmbedVideoPlugin
-from cms.models.placeholdermodel import Placeholder
+from django.test.client import RequestFactory
 from django.utils.safestring import SafeText
+
+from cms.api import create_page, add_plugin
+from cms.models.placeholdermodel import Placeholder
+from cms.plugin_rendering import ContentRenderer
+from cms.test_utils.testcases import (CMSTestCase, URL_CMS_PLUGIN_EDIT)
+
+from aldryn_video.cms_plugins import OEmbedVideoPlugin
+
 
 URL = "https://www.youtube.com/watch?v=31KMjdC6DxE"
 
@@ -35,11 +40,12 @@ class OEmbedVideoPluginTests(TestCase):
             url=self.url,
         )
         model_instance.clean()
-        html = model_instance.render_plugin({})
+        renderer = ContentRenderer(request=RequestFactory())
+        html = renderer.render_plugin(model_instance, {})
         expected_response = ('<iframe src="https://www.youtube.com/embed/31KMjdC6DxE?feature=oembed"\n'
-            '    class="iframe iframe-video"\n'
-            '    style=""></iframe>\n')
-        self.assertEqual(html.strip(), expected_response.strip())
+            '            class="embed-responsive-item"\n'
+            '            style=""></iframe>\n')
+        self.assertIn(expected_response.strip(), html.strip())
 
 
 class VideoTestCase(CMSTestCase):
@@ -48,9 +54,9 @@ class VideoTestCase(CMSTestCase):
         self.super_user = self._create_user("test", True, True)
 
     def test_plugin_edit(self):
-        page = create_page(title='page',template='page.html',language='en')
+        page = create_page(title='page',template='cms_homepage.html',language='en')
         plugin = add_plugin(
-            page.placeholders.get(slot='content'),
+            page.placeholders.get(slot='banner video'),
             OEmbedVideoPlugin,
             'en',
             url=self.url,
@@ -63,6 +69,6 @@ class VideoTestCase(CMSTestCase):
         page.publish('en')
         response = self.client.get(page.get_absolute_url('en'))
         expected_response = ('<iframe src="https://www.youtube.com/embed/31KMjdC6DxE?feature=oembed&amp;autoplay=1"\n'
-            '    class="iframe iframe-video"\n'
-            '    style="width: 560px; height: 315px;"></iframe>\n')
+            '            class="embed-responsive-item"\n'
+            '            style="width: 560px; height: 315px;"></iframe>\n')
         self.assertContains(response, expected_response)
