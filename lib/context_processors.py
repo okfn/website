7ef86a707e29f03cff1fe85a8143cc50011e736a
Adrià Mercader <amercadero@gmail.com>
2022-02-16 15:52:52 +0100

    Add plausible script for analytics (#650)
    Add plausible script for analytics for staging environment
    Co-authored-by: Andres Vazquez <andres@data99.com.ar>

--- lib/context_processors.py
+++ lib/context_processors.py
@@ -3,7 +3,21 @@ from django.contrib.sites.models import Site
 
 
 def site(request):
-    return {'site': Site.objects.get_current()}
+    host = request.get_host()
+    if host in ["127.0.0.1:8000", "localhost:8000"]:
+        environment = 'DEVELOP'
+    elif host == "next.okfn.org":
+        environment = 'STAGING'
+    elif host == "okfn.org":
+        environment = 'PRODUCTION'
+    else:
+        environment = 'UNKNOWN'
+
+    return {
+        'site': Site.objects.get_current(),
+        'environment': environment,
+        'host': host
+    }
 
 
 def google_analytics(request):
