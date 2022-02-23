3385aa7de3a7acd172de39bb47bd7ab491167620
Andres Vazquez <andres@data99.com.ar>
2022-02-23 08:53:52 -0300

    Remove Google Analytics

--- lib/context_processors.py
+++ lib/context_processors.py
@@ -20,21 +20,6 @@ def site(request):
     }
 
 
-def google_analytics(request):
-    """
-    Add the Google Analytics tracking ID and domain to the context for use when
-    rendering tracking code.
-    """
-    ga_tracking_id = getattr(settings, 'GOOGLE_ANALYTICS_TRACKING_ID', False)
-    ga_domain = getattr(settings, 'GOOGLE_ANALYTICS_DOMAIN', False)
-    if not settings.DEBUG and ga_tracking_id and ga_domain:
-        return {
-            'GOOGLE_ANALYTICS_TRACKING_ID': ga_tracking_id,
-            'GOOGLE_ANALYTICS_DOMAIN': ga_domain,
-        }
-    return {}
-
-
 def mailchimp(request):
     """
     Add the Mailchimp mailing list url and token to the context for use when
