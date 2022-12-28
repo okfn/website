2448e5b801ddfb3c31cede0db06b0f1e058544e8
avdata99 <andres@data99.com.ar>
2022-12-28 11:30:02 -0300

    remove unused Opengraph

--- foundation/organisation/utils.py
+++ foundation/organisation/utils.py
@@ -1,21 +1,8 @@
 import re
-from opengraph import opengraph
 
 from django.http import JsonResponse
 
 from foundation.organisation.models import NowDoing
-
-
-def extract_ograph_title(text):
-    text_without_hashtag = ' '.join(text.split(' ')[1:])
-    url_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]' \
-                  + r'|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-    urls = re.findall(url_pattern, text_without_hashtag)
-    if urls:
-        content = opengraph.OpenGraph(url=urls[0])
-        title = content.get('title', text_without_hashtag)
-        return urls[0], title
-    return None, text_without_hashtag
 
 
 def get_activity(text):
