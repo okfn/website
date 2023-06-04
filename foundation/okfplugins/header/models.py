46e6f736f21e03752702e11790ebd40806e413a1
Edgar Zanella Alvarenga <e@vaz.io>
2023-06-04 15:26:37 +0200

    changes in feature block and header models and fix newsletter form in all pages

--- foundation/okfplugins/header/models.py
+++ foundation/okfplugins/header/models.py
@@ -3,13 +3,15 @@ from cms.models.pluginmodel import CMSPlugin
 
 HEADER_CHOICES = (
     ('h1', 'Text top / Image left / Long text right'),
-    ('h2', 'Image left / Text right'),
+    ('h2', 'Image right / Two part text left'),
+    ('h3', 'Image left / Text right'),
 )
 
 
 class Header(CMSPlugin):
     title = models.CharField(max_length=100)
     text = models.CharField(max_length=200)
+    second_text = models.CharField(max_length=200, blank=True)
     image_alt = models.CharField(max_length=200)
     image = models.ImageField(upload_to='headers/images', blank=True)
     header_type = models.CharField(max_length=3, choices=HEADER_CHOICES, default='h1')
