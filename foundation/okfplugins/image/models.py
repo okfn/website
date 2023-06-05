da284b963c993003aafb5791a65a00618abd107e
Edgar Zanella Alvarenga <e@vaz.io>
2023-06-05 15:59:31 +0200

    Make it possible to add multiple images in grid columns

--- foundation/okfplugins/image/models.py
+++ foundation/okfplugins/image/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from cms.models.pluginmodel import CMSPlugin
 
 class OKImage(CMSPlugin):
-    tag = models.CharField(max_length=200)
+    tag = models.CharField(max_length=200, blank=True)
     image_url = models.ImageField(upload_to='video/images')
-    text = models.CharField(max_length=400, default='')
-    more_text = models.CharField(max_length=400, default='')
-    url = models.CharField(max_length=400, default='')
-    caption = models.CharField(max_length=400, default='')
-    alt = models.CharField(max_length=400, default='')
+    text = models.CharField(max_length=400, default='', blank=True)
+    more_text = models.CharField(max_length=400, default='', blank=True)
+    url = models.CharField(max_length=400, default='', blank=True)
+    caption = models.CharField(max_length=400, default='', blank=True)
+    alt = models.CharField(max_length=400, default='', blank=True)
+    in_column = models.BooleanField(default=False)
 
     def __str__(self):
         return self.text
