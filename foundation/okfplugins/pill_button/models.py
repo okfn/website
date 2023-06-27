59093ff6391af1a551f521543ae9f91da732be6b
Edgar Zanella Alvarenga <e@vaz.io>
2023-06-27 18:14:40 +0200

    Add new types of buttons

--- foundation/okfplugins/pill_button/models.py
+++ foundation/okfplugins/pill_button/models.py
@@ -5,6 +5,8 @@ BUTTON_TYPE = (
     ('black', 'Black'),
     ('white', 'White'),
     ('pill', 'Pill'),
+    ('subtitle', 'Subtitle'),
+    ('subtitle_large', 'Subtitle large'),
     ('arrow', 'Arrow')
 )
 
@@ -12,7 +14,7 @@ class PillButton(CMSPlugin):
     text = models.CharField(max_length=200)
     url = models.CharField(max_length=200, blank=True)
     another_window = models.BooleanField(default=False)
-    button_type = models.CharField(max_length=6, choices=BUTTON_TYPE, default='pill')
+    button_type = models.CharField(max_length=16, choices=BUTTON_TYPE, default='pill')
 
     def __str__(self):
         return self.text
