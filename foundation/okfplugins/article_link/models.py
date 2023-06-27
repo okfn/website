f9fa84333d49254ee195e4be76b7ab03dd1119e9
Edgar Zanella Alvarenga <e@vaz.io>
2023-06-27 20:06:08 +0200

    Make article link not mandatory

--- foundation/okfplugins/article_link/models.py
+++ foundation/okfplugins/article_link/models.py
@@ -6,7 +6,7 @@ class ArticleLink(CMSPlugin):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     image = models.ImageField(upload_to="article/images", blank=True)
-    url = models.CharField(max_length=400, default="")
+    url = models.CharField(max_length=400, default="", blank=True)
 
     def __str__(self):
         return self.title
