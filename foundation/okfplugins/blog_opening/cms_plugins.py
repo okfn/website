a626d77e75a77cce8c0f24e47884ae9609cbf92b
Edgar Zanella Alvarenga <e@vaz.io>
2023-06-27 18:30:34 +0200

    Remove unused classes from blog_opening and hero_punch plugins

--- foundation/okfplugins/blog_opening/cms_plugins.py
+++ foundation/okfplugins/blog_opening/cms_plugins.py
@@ -1,6 +1,5 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
 
 from .models import BlogOpening
