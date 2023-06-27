6da65642bb01ec4cda78c2ddacdb08fe9dc69ba5
Edgar Zanella Alvarenga <e@vaz.io>
2023-06-27 11:53:37 +0200

    Add aligment options to heading plugin

--- foundation/okfplugins/heading/models.py
+++ foundation/okfplugins/heading/models.py
@@ -10,10 +10,17 @@ HEADINGS_CHOICES = (
     ('h5','h6'),
 )
 
+HEADINGS_ALIGNMENTS = (
+    ("center", "Center"),
+    ("left", "Left"),
+    ("right", "Right")
+)
+
 class Heading(CMSPlugin):
     title = models.CharField(max_length=200)
     heading_type = models.CharField(max_length=3, choices=HEADINGS_CHOICES, default='h1')
     highlighted  = models.BooleanField(default=False)
+    alignment = models.CharField(max_length=6, choices=HEADINGS_ALIGNMENTS, default="center")
 
     def __str__(self):
         return self.title
