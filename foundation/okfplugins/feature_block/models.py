84aa6432633fcec9bddcbf1f83ffd20531b94403
Edgar Zanella Alvarenga <e@vaz.io>
2023-06-29 14:44:07 +0200

    Add new formats for feature block

--- foundation/okfplugins/feature_block/models.py
+++ foundation/okfplugins/feature_block/models.py
@@ -7,12 +7,16 @@ BLOCK_CHOICES = (
     ("white", "White"),
     ("transparent_title", "Transparent with Title"),
     ("transparent", "Transparent"),
+    ("background_rounded","Rounded corners"),
+    ("blue", "Blue Background"),
+    ("yellow", "Yellow Background"),
+    ("purple", "Purple Background"),
 )
 
 
 class FeatureBlock(CMSPlugin):
-    title = models.CharField(max_length=200)
-    text = models.CharField(max_length=400)
+    title = models.CharField(max_length=200, blank=True)
+    text = models.CharField(max_length=400, blank=True)
     date = models.CharField(max_length=200, default="", blank=True)
     date_number = models.CharField(max_length=200, default="", blank=True)
     block_type = models.CharField(
