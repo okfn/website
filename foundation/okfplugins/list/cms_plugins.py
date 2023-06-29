a76580eb31c30a1fadf9c7aa951bef901ac3dfa1
Edgar Zanella Alvarenga <e@vaz.io>
2023-06-29 15:36:52 +0200

    Add list type plugin

---
+++ foundation/okfplugins/list/cms_plugins.py
@@ -0,0 +1,21 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.utils.translation import ugettext_lazy as _
+
+from .models import List
+
+
+@plugin_pool.register_plugin
+class ListPlugin(CMSPluginBase):
+    model = List
+    module = "OKF v2"
+    render_template = "list_plugin.html"
+    cache = False
+    name = _("List")
+
+    def render(self, context, instance, placeholder):
+        instance.list_items = instance.items.splitlines()  
+        context = super().render(context, instance, placeholder)
+
+        return context
+
