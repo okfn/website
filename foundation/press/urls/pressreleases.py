085dae79d8fe616434bef107b3fac1c165541256
avdata99 <andres@data99.com.ar>
2023-07-05 11:42:39 -0300

    flake8 fix

--- foundation/press/urls/pressreleases.py
+++ foundation/press/urls/pressreleases.py
@@ -5,6 +5,8 @@ from ..views import PressReleaseListView, PressReleaseDetailView
 
 urlpatterns = [
     re_path(r'^$', PressReleaseListView.as_view(), name='press-releases'),
-    re_path(r'^(?P<slug>[^/]+)/$', PressReleaseDetailView.as_view(),
-        name='press-release'),
+    re_path(
+        r'^(?P<slug>[^/]+)/$', PressReleaseDetailView.as_view(),
+        name='press-release'
+    ),
 ]
