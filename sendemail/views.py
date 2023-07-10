e1d51142453ee5cafb581e17e0d58f6632eaa65c
pdelboca <patriciodelboca@gmail.com>
2023-07-10 11:02:29 +0200

    Refactor email structure

--- sendemail/views.py
+++ sendemail/views.py
@@ -16,23 +16,22 @@ def contactview(request):
         messages.error(request, 'Required information is missing')
         return render(request, "cms_contact.html", {'form': form})
 
-    name = form.cleaned_data['name']
-    email = form.cleaned_data['email']
-    message = form.cleaned_data['message']
-    organisation = form.cleaned_data['organisation']
+    how_can_we_help = form.cleaned_data['how_can_we_help']
+    email_subject = 'New contact from OKFN website: ' + how_can_we_help
 
-    # collate and send filled form data in one email
-    message_from = 'Name: ' + name + '\n' + 'From: ' + organisation
-    message_details = 'Email: ' + email + '\n' + 'Message: ' + message
-    email_message = message_from + '\n' + message_details
-    email_subject = 'Enquiry from ' + name
+    email_message = f"""
+Name: {form.cleaned_data['name']}
+From: {form.cleaned_data['organisation']}
+Email: {form.cleaned_data['email']}
+Telephone: {form.cleaned_data['telephone']}
+Message: {form.cleaned_data['message']}
+    """
 
     # flatten recepients list:
     # (('name1', 'email1'), ...) -> ('email1', ...)
     recepients = settings.GENERAL_EMAIL_RECEPIENTS
     recepients = [recepient[1] for recepient in recepients]
 
-    # all form data will originate from the same email address
     try:
         send_mail(
             email_subject, email_message,
@@ -40,7 +39,6 @@ def contactview(request):
     except BadHeaderError:
         return HttpResponse('Invalid header found.')
 
-    # pop up message once form data has been sent successfully
     messages.info(
         request,
         'Thank you for your message. '
