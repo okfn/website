60588a21427b808d4673b6d91a3841c69da15285
pdelboca <patriciodelboca@gmail.com>
2023-07-17 12:31:50 +0200

    Remove Projects AppHooks and remaining templates and classes

--- foundation/organisation/cms_apps.py
+++ foundation/organisation/cms_apps.py
@@ -1,7 +1,6 @@
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import gettext_lazy as _
-from .cms_menus import ProjectMenu
 
 
 class UnitsAppHook(CMSApp):
@@ -32,19 +31,6 @@ class AdvisoryBoardAppHook(CMSApp):
 
 
 apphook_pool.register(AdvisoryBoardAppHook)
-
-
-class ProjectsAppHook(CMSApp):
-    name = _("Projects")
-
-    def get_menus(self, page=None, language=None, **kwargs):
-        return [ProjectMenu]
-
-    def get_urls(self, page=None, language=None, **kwargs):
-        return ["foundation.organisation.urls.projects"]
-
-
-apphook_pool.register(ProjectsAppHook)
 
 
 class WorkingGroupsAppHook(CMSApp):
