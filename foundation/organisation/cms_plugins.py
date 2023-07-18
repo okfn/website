65ae8409839ad2d3f6391cbc1a576f399c4a44f1
pdelboca <patriciodelboca@gmail.com>
2023-07-18 10:18:32 +0200

    Delete NetworkGroupFlagsPlugin

--- foundation/organisation/cms_plugins.py
+++ foundation/organisation/cms_plugins.py
@@ -5,31 +5,7 @@ from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from cms.extensions.extension_pool import extension_pool
 
-from .models import (NetworkGroup, NetworkGroupList, WorkingGroup,
-                     SideBarExtension)
-
-
-class NetworkGroupFlagsPlugin(CMSPluginBase):
-    model = NetworkGroupList
-    module = "OKF"
-    name = _("Network Group Flags")
-    render_template = "organisation/networkgroup_flags.html"
-    text_enabled = True
-
-    def icon_src(self, instance):
-        return settings.STATIC_URL + "cms/img/icons/plugins/snippet.png"
-
-    def render(self, context, instance, placeholder):
-        context = super().render(context, instance, placeholder)
-
-        context['title'] = instance.get_group_type_display()
-        context['countries'] = NetworkGroup.objects.countries().filter(
-            group_type=instance.group_type).order_by('name')
-
-        return context
-
-
-plugin_pool.register_plugin(NetworkGroupFlagsPlugin)
+from .models import WorkingGroup, SideBarExtension
 
 
 class WorkingGroupPlugin(CMSPluginBase):
