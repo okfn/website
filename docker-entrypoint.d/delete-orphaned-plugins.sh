909fe5d24d9bce7ef1fcff121ed04b5f5612faf1
pdelboca <patriciodelboca@gmail.com>
2023-07-20 10:02:22 +0200

    Add execution of custom scripts in docker entrypoint

---
+++ docker-entrypoint.d/delete-orphaned-plugins.sh
@@ -0,0 +1,11 @@
+# !/bin/sh
+
+# Given foreign key restrictions, ArticleListItemPlugin we cannot rely on delete-orphaned-plugins
+# and we should uninstall it first.
+python3 manage.py cms uninstall plugins ArticleListItemPlugin --noinput
+
+# Clean remaining AppHooks left from previous removals.
+python3 manage.py cms uninstall apphooks PressReleaseAppHook ThemesAppHook --noinput
+
+# This will remove data in cms_cmsplugins table from all non-longer-existing plugins
+python3 manage.py cms delete-orphaned-plugins --noinput
