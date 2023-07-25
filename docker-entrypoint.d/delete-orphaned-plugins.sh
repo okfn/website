7583c9aa74b626a99a0c6a99e00c253a25f4f042
pdelboca <patriciodelboca@gmail.com>
2023-07-25 09:51:50 +0200

    Uninstall WorkingGroupAppHook

--- docker-entrypoint.d/delete-orphaned-plugins.sh
+++ docker-entrypoint.d/delete-orphaned-plugins.sh
@@ -6,6 +6,7 @@ python3 manage.py cms uninstall plugins ArticleListItemPlugin --noinput
 
 # Clean remaining AppHooks left from previous removals.
 python3 manage.py cms uninstall apphooks PressReleaseAppHook ThemesAppHook --noinput
+python3 manage.py cms uninstall apphooks WorkingGroupsAppHook --noinput
 
 # This will remove data in cms_cmsplugins table from all non-longer-existing plugins
 python3 manage.py cms delete-orphaned-plugins --noinput
