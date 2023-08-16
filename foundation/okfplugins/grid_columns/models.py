8a70887d2666236be193d27d44698214044c0a4a
shashi gharti <shashi.gharti@gmail.com>
2023-08-16 10:35:00 +0545

    Add skip column feature

--- foundation/okfplugins/grid_columns/models.py
+++ foundation/okfplugins/grid_columns/models.py
@@ -5,6 +5,7 @@ from cms.models.pluginmodel import CMSPlugin
 class GridColumns(CMSPlugin):
     title = models.CharField(max_length=200)
     columns = models.IntegerField(default=2)
+    skip_columns = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
