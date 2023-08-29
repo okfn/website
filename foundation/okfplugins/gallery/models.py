4a95ca9f405f8bbfd7ddb6204c930f332bc0b926
shashi gharti <shashi.gharti@gmail.com>
2023-08-29 11:44:49 +0545

    Fixed formatting

--- foundation/okfplugins/gallery/models.py
+++ foundation/okfplugins/gallery/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from ..background.models import AbstractBackgroundPlugin
 
+
 class Gallery(AbstractBackgroundPlugin):
     title = models.CharField(max_length=200, blank=True)
 
