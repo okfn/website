9b7f6cf219c2b5e77d36ae207fcba0020db87327
shashi gharti <shashi.gharti@gmail.com>
2023-09-06 14:36:39 +0545

    Adds single page view for member Adds links to header in search result for person

--- foundation/organisation/urls/units.py
+++ foundation/organisation/urls/units.py
@@ -2,7 +2,9 @@ from django.urls import re_path
 from django.views.generic.list import ListView
 
 from ..models import Unit
+from ..views import PersonView
 
 urlpatterns = [
     re_path(r'^$', ListView.as_view(model=Unit), name='units'),
+    re_path(r'^(?P<person_id>\d+)/$', PersonView.as_view(), name='person')
 ]
