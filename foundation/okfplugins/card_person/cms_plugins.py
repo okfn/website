7a8e99026e5acdf34b0741b4302a50c099139baa
pdelboca <patriciodelboca@gmail.com>
2023-11-29 12:24:21 +0100

    Add card_person plugin

---
+++ foundation/okfplugins/card_person/cms_plugins.py
@@ -0,0 +1,17 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.utils.translation import gettext_lazy as _
+
+from .models import CardPerson
+
+@plugin_pool.register_plugin
+class CardPersonPlugin(CMSPluginBase):
+    model = CardPerson
+    module = "OKF v2"
+    name = _("Card Person Plugin")
+    render_template = "card_person.html"
+    cache = False
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
