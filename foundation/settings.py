92822cae384adfea5adab4c89edb37eefc76ac61
pdelboca <patriciodelboca@gmail.com>
2023-12-01 11:40:48 +0100

    Remove django-compressor since our assets are minified by npm

--- foundation/settings.py
+++ foundation/settings.py
@@ -124,9 +124,6 @@ INSTALLED_APPS = (
     'sendemail',
     'captcha',
 
-    # Asset pipeline
-    'compressor',
-
     # CMS plugins
     'djangocms_picture',
     'djangocms_text_ckeditor',
@@ -311,7 +308,6 @@ USE_TZ = True
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
 )
 
 # Where else to find static files
@@ -437,19 +433,6 @@ CSP_CONNECT_SRC = asset_hosts + [
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/report-uri
 # CSP_REPORT_URI = env.get('DJANGO_CSP_REPORT_URI')
 # CSP_REPORT_ONLY = True
-
-COMPRESS_OFFLINE = env.get('DJANGO_COMPRESS_OFFLINE') == 'true'
-COMPRESS_OFFLINE_CONTEXT = {
-    'STATIC_URL': STATIC_URL,
-}
-
-COMPRESS_PRECOMPILERS = (
-    ('text/sass', 'lib.precompilers.SassFilter'),
-)
-
-COMPRESS_FILTERS = {
-    'css': ['compressor.filters.cssmin.CSSMinFilter']
-}
 
 CMS_CACHE_DURATIONS = {
     'content': 60,
